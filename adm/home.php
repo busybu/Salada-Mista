14ae9ac954ca6912328016f9d720fe4111eb49cf
lucas <lucas@gmail.com>
2022-11-25 16:38:57 -0300

    home admin adicionada

--- adm/home.php
+++ adm/home.php
@@ -14,11 +14,32 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/admhome.css">
     <title>Bem vindo
         <?php echo $_SESSION['loginUser']; ?>
     </title>
 </head>
 <body>
-    <div>Coco</div>
+    <header>
+        <div class="header-box">
+            <p>Olá,
+                <?php echo $_SESSION['loginUser']; ?>
+            </p>
+            <img src="../img/5.png" alt="">
+        </div>
+    </header>
+    <section class="options-container">
+        <div class="options-box">
+            <div class="options">
+                <button>Postagens</button>
+                <button>Perguntas</button>
+                <button>Usuários</button>
+                <button>Sair</button>
+            </div>
+            <div class="img-box">
+                
+            </div>
+        </div>
+    </section>
 </body>
 </html>
